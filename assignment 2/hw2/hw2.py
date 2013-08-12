c8a13fdcb71d8224e765363355c7a04eb1c360f1
Dhruv Gairola <dhruv.gairola.89@gmail.com>
2013-08-12 19:56:47 -0400

    Finished assignment 3.

--- assignment 2/hw2/hw2.py
+++ assignment 2/hw2/hw2.py
@@ -2,7 +2,7 @@
 # Please fill out this stencil and submit using the provided submission script.
 
 from vec import Vec
-
+from GF2 import one
 
 
 ## Problem 1
@@ -16,7 +16,17 @@ def vec_select(veclist, k):
     >>> vec_select([v1, v2, v3, v4], 'a') == [Vec(D,{'b': 1}), Vec(D,{'b': 2})]
     True
     '''
-    pass
+
+    selectedList = []
+    for v in veclist:
+        if k in v.f.keys():
+            if v.f[k] == 0:
+                selectedList.append(v)
+        else:
+            selectedList.append(v)
+
+    return selectedList
+
 
 def vec_sum(veclist, D): 
     '''
@@ -28,7 +38,17 @@ def vec_sum(veclist, D):
     >>> vec_sum([v1, v2, v3, v4], D) == Vec(D, {'b': 13, 'a': 11})
     True
     '''
-    pass
+
+    retMap = {}
+
+    for v in veclist:
+        for key, val in v.f.items():
+            if key in retMap.keys():
+                retMap[key] += val
+            else:
+                retMap[key] = val
+
+    return Vec(D, retMap)
 
 def vec_select_sum(veclist, k, D): 
     '''
@@ -40,7 +60,8 @@ def vec_select_sum(veclist, k, D):
     >>> vec_select_sum([v1, v2, v3, v4], 'a', D) == Vec(D, {'b': 3})
     True
     '''
-    pass
+
+    return vec_sum(vec_select(veclist, k), D)
 
 
 
@@ -52,7 +73,15 @@ def scale_vecs(vecdict):
     >>> scale_vecs({3: v1, 5: v2}) == [Vec({1,2,3},{2: 3.0}), Vec({1,2,4},{1: 0.2, 2: 0.4, 4: 1.6})]
     True
     '''
-    pass
+
+    scaledList = []
+    for key,val in vecdict.items():
+        scaledVec = {}
+        for k,v in val.f.items():
+            scaledVec[k] = v/key
+        scaledList.append(Vec(val.D, scaledVec))
+
+    return scaledList
 
 
 
@@ -73,24 +102,36 @@ def GF2_span(D, L):
     >>> Vec(D, {x:one for x in D}) in GF2_span(D, L)
     True
     '''
-    pass
+
+    if not L:
+        return [Vec(D, {})]
+    else:
+        first = L[0]
+        rest = L[1:]
+        span = GF2_span(D, rest)
+
+        ones = [one * first + v for v in span]
+        zeros = [0 * first + v for v in span]
+
+        return ones + zeros
+
 
 
 
 ## Problem 4
 # Answer with a boolean, please.
 
-is_it_a_vector_space_1 = ...
-is_it_a_vector_space_2 = ...
+is_it_a_vector_space_1 = True
+is_it_a_vector_space_2 = False
 
 
 
 ## Problem 5
-is_it_a_vector_space_3 = ...
-is_it_a_vector_space_4 = ...
+is_it_a_vector_space_3 = True
+is_it_a_vector_space_4 = False
 
 
 ## Problem 6
 
-is_it_a_vector_space_5 = ...
-is_it_a_vector_space_6 = ...
+is_it_a_vector_space_5 = True
+is_it_a_vector_space_6 = False
